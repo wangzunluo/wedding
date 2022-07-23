6d527aa762305023d56fdc85efb70253ea803df7
unknown <wangarwin13@gmail.com>
2022-07-23 00:35:17 -0500

    add db and form saving logic

--- main/app.py
+++ main/app.py
@@ -1,19 +1,26 @@
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+import os
 
+
+cwd = os.getcwd().replace('\\', '/')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+cwd[3:]+'/db/test.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    fname = db.Column(db.String(80), unique=False, nullable=False)
+    lname = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(20), unique=True, nullable=True)
+    food = db.Column(db.String(20), unique=False, nullable=False)
+
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '{{0}} {{1}}'.format(self.fname, self.lname)
 
 @app.route('/')
 def landing():
@@ -24,11 +31,16 @@ def landing():
 def countdown():
     return render_template('countdown.html')
 
-@app.route('/rsvp')
-@app.route('/rsvp/<test>')
-def rsvp(test=None):
-    print(test)
-    return render_template('rsvp.html')
+@app.route('/rsvp', methods=['GET', 'POST'])
+def rsvp():
+    if request.method =='GET':
+        print('test')
+        return render_template('rsvp.html')
+    elif request.method == 'POST':
+        num = request.form['people']
+        print(num)
+        return redirect(url_for('rsvp')+'/'+str(num))
 
-def test():
-    print("test")
+@app.route('/rsvp/<num>')
+def rsvp_num(num=None):
+    return render_template('rsvp.html', num=num)
