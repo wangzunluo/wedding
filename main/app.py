1d347aff0ce15806c00ba475e5bdf98f48abbd3b
unknown <wangarwin13@gmail.com>
2022-08-04 22:23:55 -0500

    I'm too lazy to write a good commit message

--- main/app.py
+++ main/app.py
@@ -1,4 +1,5 @@
 
+from mimetypes import init
 from flask import Flask, redirect, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 import os
@@ -15,22 +16,35 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fname = db.Column(db.String(80), unique=False, nullable=False)
     lname = db.Column(db.String(80), unique=False, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=True)
     phone = db.Column(db.String(20), unique=True, nullable=True)
     food = db.Column(db.String(20), unique=False, nullable=False)
 
-
     def __repr__(self):
-        return '{{0}} {{1}}'.format(self.fname, self.lname)
+        return '{0} {1}'.format(self.fname, self.lname)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def landing():
+    if request.method == 'POST':
+        print('start', file=sys.stdout, flush=True)
+        users = User.query.all()
+        users.sort(reverse=True, key=lambda user : user.fname)
+        print(users, file=sys.stdout, flush=True)
+        print('done', file=sys.stdout, flush=True)
     return render_template('landing.html')
 
 
 @app.route('/countdown')
 def countdown():
     return render_template('countdown.html')
+
+
+@app.route('/guests')
+def guests():
+    users = User.query.all()
+    users.sort(key=lambda user : user.fname.lower())
+    return render_template('guests.html', users=users)
+
 
 @app.route('/rsvp', methods=['GET', 'POST'])
 def rsvp():
@@ -48,7 +62,7 @@ def rsvp_num(num=None):
     if request.method == 'GET':
         return render_template('rsvp.html', num=int(num))
     elif request.method == 'POST':
-        print(request.form, file=sys.stdout, flush=True)
+        print(request.form, flush=True)
         form = request.form
         fnames = form.getlist('fname')
         lnames = form.getlist('lname')
@@ -62,6 +76,17 @@ def rsvp_num(num=None):
         print(email, file=sys.stdout, flush=True)
         print(phone, file=sys.stdout, flush=True)
         print(foods, file=sys.stdout, flush=True)
-        return render_template('success.html', name='generic')
+        
+        person = User(fname=fnames[0], lname=lnames[0], food=foods[0])
+        db.session.add(person)
+        counter = 1
+        while counter < len(fnames):
+            person = User(fname=fnames[counter], lname=lnames[counter], food=foods[counter])
+            db.session.add(person)
+            counter += 1
+        db.session.commit()
+        print(User.query.all(), file=sys.stdout, flush=True)
+
+        return render_template('success.html', name=fnames[0])
     else:
         return render_template('error.html', method=request.method)
