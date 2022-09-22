6fadffd4e1853b0f183754c528fb0c4578c660a2
unknown <wangarwin13@gmail.com>
2022-09-22 17:03:51 -0500

    compress images

--- main/static/js/gallery.js
+++ main/static/js/gallery.js
@@ -105,6 +105,10 @@ let itemData = [
   {img: "static/gallery/1.jpg", rows: 5, cols: 2},
 ]
 
+itemData.forEach(item => {
+  item['img'] = item['img'].slice(0,-4) + "-min.jpg"
+})
+
 function srcset(image, size, rows = 1, cols = 1) {
   return {
     src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
